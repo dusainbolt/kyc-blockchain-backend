18223821097ac3d77e0222883096a3aa27ed1f21
= <cuong06012000htk@gmail.com>
2022-03-12 12:29:42 +0700

    cuong_api-function-update-kyc

--- repositories/kyc_repository.js
+++ repositories/kyc_repository.js
@@ -10,4 +10,13 @@ module.exports = {
       _logger.error(new Error(error));
     }
   },
+  updateOne: async function (id, newData) {
+    try {
+      // update and return the result
+      const updateResult = await KycModel.updateOne(id, { $set: newData });
+      return updateResult;
+    } catch (error) {
+      _logger.error(new Error(error));
+    }
+  },
 };
